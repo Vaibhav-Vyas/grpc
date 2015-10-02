2ca3c07dba351217858864337ec302b976f9bc0c
Vaibhav-Vyas <vaibhavvyas@cs.wisc.edu>
2015-10-01 21:54:12 -0500

    Benchmark Roundtrip time of GRPC library for small message size.

--- examples/cpp/helloworld/greeter_client.cc
+++ examples/cpp/helloworld/greeter_client.cc
@@ -38,6 +38,7 @@
 #include <grpc++/grpc++.h>
 
 #include "helloworld.grpc.pb.h"
+#include "./benchmarkUtils.h"
 
 using grpc::Channel;
 using grpc::ClientContext;
@@ -53,7 +54,10 @@ class GreeterClient {
 
   // Assambles the client's payload, sends it and presents the response back
   // from the server.
-  std::string SayHello(const std::string& user) {
+  std::string SayHello(const std::string& user,
+                       struct timespec *pStart,
+                       struct timespec *pEnd,
+                       uint64_t *pDiff) {
     // Data we are sending to the server.
     HelloRequest request;
     request.set_name(user);
@@ -65,8 +69,14 @@ class GreeterClient {
     // the server and/or tweak certain RPC behaviors.
     ClientContext context;
 
+    // Measuring RPC msg overhead.
+    clock_gettime(CLOCK_MONOTONIC, pStart);
+
     // The actual RPC.
-    Status status = stub_->SayHello(&context, request, &reply);
+    Status status = stub_->SayHello(&context, request, &reply); 
+
+    clock_gettime(CLOCK_MONOTONIC, pEnd);
+    *pDiff = diffTime(*pStart, *pEnd);
 
     // Act upon its status.
     if (status.ok()) {
@@ -80,16 +90,79 @@ class GreeterClient {
   std::unique_ptr<Greeter::Stub> stub_;
 };
 
+//! Function to measure the Round Trip time of GRPC with small message size.
+int
+measureRoundTripTime(GreeterClient *pGreeter,
+    char *pServIPPort, 
+    int gccOptimizationMode,
+    int maxAttempts);
+
 int main(int argc, char** argv) {
+  int gccOptimizationMode;
+  int maxAttempts = 10;
+  char servIPPort[25] = {0};
+  int rc = 0;
+
+  if(argc != 3)
+  {
+          printf("\n Usage: %s <ip of server:port> <optimization_enabled ? 1 : 0>\n", argv[0]);
+          return 1;
+  }
+  else
+  {
+      printf("IP =%s", argv[1]);
+      sprintf(servIPPort, "%s __", argv[1]);
+      servIPPort[strlen(argv[1])] = '\0';
+      //strcpy(servIPPort, "%s\0", argv[1]);
+      gccOptimizationMode = atoi(argv[2]);
+  }
+
   // Instantiate the client. It requires a channel, out of which the actual RPCs
   // are created. This channel models a connection to an endpoint (in this case,
   // localhost at port 50051). We indicate that the channel isn't authenticated
   // (use of InsecureCredentials()).
   GreeterClient greeter(
-      grpc::CreateChannel("localhost:50051", grpc::InsecureCredentials()));
-  std::string user("world");
-  std::string reply = greeter.SayHello(user);
-  std::cout << "Greeter received: " << reply << std::endl;
+      grpc::CreateChannel(servIPPort, grpc::InsecureCredentials()));
 
-  return 0;
+  rc = measureRoundTripTime(&greeter,
+                       servIPPort,
+                       gccOptimizationMode,
+                       maxAttempts); 
+}
+
+int
+measureRoundTripTime(GreeterClient *pGreeter,
+    char *pServIPPort, 
+    int gccOptimizationMode,
+    int maxAttempts)
+{
+    // For Timing measurement.
+    struct timespec start; 
+    struct timespec end;
+    uint64_t diffNanoSec;
+    int run;
+    vector<string> strMsg;
+    std::string user("world");
+    char experimentTitle[64];
+
+
+    sprintf(experimentTitle,"Q4_RoundTripCalc_OptimzationMode_%d_RTT_SmallMsg_IP_%s",
+        gccOptimizationMode, pServIPPort);
+
+    cout << "================================================" << std::endl;
+    cout << "4) Measuring GRPC roundtrip time (small msg => string msg." << std::endl;
+    cout << "================================================" << std::endl;
+    for (run = 0; run < maxAttempts; run++)
+    {
+      std::string reply = pGreeter->SayHello(user, &start, &end, &diffNanoSec);
+      monotonicClk[run] = (uint64_t) diffNanoSec;
+
+      //std::cout << "Greeter received: " << reply << std::endl;
+      printf("\t * RTT Attempts: %d), String Type, Sent Msg=,%s, Received Msg=,%s, Time taken = ,%llu,nanoseconds\n",
+              run, user.c_str(), reply.c_str(), 
+             (long long unsigned int)diffNanoSec);
+    }
+    createCSVReport(experimentTitle, maxAttempts, &strMsg);
+
+    return 0;
 }
