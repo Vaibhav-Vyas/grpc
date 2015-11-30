d483e23fa054a4b1d74815630d446d3f88c1a74f
Vaibhav Vyas <vaibhav.n.vyas@gmail.com>
2015-11-29 22:29:29 -0600

    Add new bash script to compile the source and then regenerate the GRPC.CC and PB.CC files. Default makefile was deleting the files generated by Protoc.

---
+++ examples/cpp/helloworld/make_with_proto_cc.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+make
+protoc -I ../../protos --cpp_out=. ../../protos/helloworld.proto
+protoc -I ../../protos --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ../../protos/helloworld.proto
